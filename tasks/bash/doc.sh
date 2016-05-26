efd06f7e025da25c67ac8baf4c1af4e8d6724186
Richard King <richrdkng@gmail.com>
2016-05-26 23:48:32 +0200

    Update "doc" script
    - finalize functions, case namings and documentation

--- tasks/bash/doc.sh
+++ tasks/bash/doc.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-# info: https://brendancleary.com/2013/03/08/including-a-github-wiki-in-a-repository-as-a-submodule/
+# |---------------------------------------------------------------------------------------------------------------------
+# |
+# | Documentation Manager Script
+# |
+# |     - Before you make **any** changes, in order to understand the means, when using GitHub's repository wiki
+# |       as a git submodule serving as the projects documentation in the projects /doc folder,
+# |       **read thoroughly and understand the following article**:
+# |       https://brendancleary.com/2013/03/08/including-a-github-wiki-in-a-repository-as-a-submodule/
+# |
+# |---------------------------------------------------------------------------------------------------------------------
+
 
 function add-wiki-repo-as-submodule {
+
+    # check **Setup – Clone submodule into existing repo** section on
+    # https://brendancleary.com/2013/03/08/including-a-github-wiki-in-a-repository-as-a-submodule/
+
+    # filter the wiki's repository url
     wiki=$(node -p -e '
         "use strict";
 
@@ -13,86 +28,95 @@ function add-wiki-repo-as-submodule {
             .replace(/.git$/g, ".wiki.git")
     ')
 
-    # make sure we are ath the right path
+    # make sure we are at the right path
     cd /vagrant
 
     # remove /doc, if it is present
     rm -rf doc
 
+    # add the wiki's repository as a git submodule
     git submodule add $wiki doc
 
-    git commit -m "Add repository's wiki repo as a submodule in /doc"
-
+    # it is necessary to commit and then push the modified git repository
+    # after a submodule was added to the project's repository
+    git commit -m "Add the project's wiki repository as a git submodule into /doc"
     git push
 
+    # with this way, pull the submodule's content for the first time
     git submodule init
-
     git submodule update
 }
 
-function pull-wiki-submodul-changes {
+function pull-wiki-submodule-changes {
+
+    # check **Update the docs Submodule** section on
+    # https://brendancleary.com/2013/03/08/including-a-github-wiki-in-a-repository-as-a-submodule/
+
     cd /vagrant
 
+    # pull and merge changes to the submodule in order to properly update the submodule's contents
     git pull
-
     git merge origin/master
-
     git submodule update
 }
 
-function update-wiki-submodule {
+function commit+push-wiki-submodule {
     name=$(node -p -e "require('./package.json').name")
+
+    # check **Make changes to the docs submodule** section on
+    # https://brendancleary.com/2013/03/08/including-a-github-wiki-in-a-repository-as-a-submodule/
 
     cd /vagrant/doc
 
     git checkout master
-
     git commit -am "Update $name API documentation"
-
-    git push
-
-    cd /vagrant
-
-    git commit -am "Update doc submodule"
-
     git push
 }
 
-function remove-wiki-submodule {
+function commit+push-project {
+    name=$(node -p -e "require('./package.json').name")
+
+    # check **Make changes to the docs submodule** section on
+    # https://brendancleary.com/2013/03/08/including-a-github-wiki-in-a-repository-as-a-submodule/
+
     cd /vagrant
-
-    git rm doc
-
-    rm -rf .git/modules/doc
+    git commit -am "Update $name API documentation"
+    git push
 }
 
 function wiki-submodule-status {
     cd /vagrant/doc
-
     git status
 }
 
 case $1 in
-    init)
+    # run it via "npm run doc -- init-submodule"
+    init-submodule|init)
         add-wiki-repo-as-submodule
         ;;
 
-    pull)
-        pull-wiki-submodul-changes
+    # run it via "npm run doc -- pull-submodule"
+    pull-submodule|pull)
+        pull-wiki-submodule-changes
         ;;
 
-    update)
-        update-wiki-submodule
+    # run it via "npm run doc -- push-submodule"
+    push-submodule|push)
+        commit+push-wiki-submodule
         ;;
 
-    remove)
-        remove-wiki-submodule
-        ;;
-
-    status)
+    # run it via "npm run doc -- status-submodule"
+    status-submodule|status)
         wiki-submodule-status
         ;;
 
+    # run it via "npm run doc -- push-submodule-then-push-project"
+    push-submodule-push-project)
+        commit+push-wiki-submodule
+        commit+push-project
+        ;;
+
+    # run it via "npm run doc"
     *)
         npm run gulp -- doc
         ;;
