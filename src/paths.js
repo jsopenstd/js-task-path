68ad3ab776aae6c4dbc0efc0984b17672c8d5bca
Richard King <richrdkng@gmail.com>
2016-05-28 09:48:36 +0200

    v1.0.2

--- src/paths.js
+++ src/paths.js
@@ -1,6 +1,6 @@
 /**
  * @overview A utility singleton to help manage task-related paths more easily throughout the whole project.
- * @version 1.0.1
+ * @version 1.0.2
  *
  * @author Richard King <richrdkng@gmail.com> [GitHub]{@link https://github.com/richrdkng}
  * @licence MIT
